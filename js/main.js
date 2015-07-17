d0f1de8cca687d22cb906b595b2e1e2806bfa466
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-07-17 22:47:45 +0100

    Added initial implementation of contact form.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+$(document).ready(function () {
 
+	$('form').submit(function (event) {
+
+		var base64_email = 'amNiQGpvc2VwaGJpbGxpbmdzbGV5LmNvLnVr';
+		var base_url = '//forms.brace.io/';
+		var action = base_url + atob(base64_email);
+
+		alert(action);
+		alert($('input[name=name]').val());
+
+		$.ajax({
+			url: action,
+			method: "POST",
+			data: {
+				message: $('input[name=name]').val()
+			},
+			dataType: "json"
+		});
+
+		event.preventDefault();
+	});
+});
