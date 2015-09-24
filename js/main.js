32afd3b40f94f8c18b09476eeef3605ec9084172
Joe Billingsley <jb@josephbillingsley.co.uk>
2015-09-24 17:30:58 +0100

    Added code to make evolutionary computing text evolve.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,87 @@
-/// <reference path="../typings/jquery/jquery.d.ts"/>
-$(document).ready(function () {
+var evolvingElements = document.getElementsByClassName("evolve");
 
-	$('#people-quotes').owlCarousel({
-		navigation: true,
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		items: 3
-	});
+var initialWait = 5;
+var maxWait = 150;
 
-	$('form').submit(function (event) {
+for(var i = 0; i < evolvingElements.length; i++) {	
+	evolveTo(evolvingElements[i].innerHTML, evolvingElements[i]);
+}
 
-		var base64_email = 'amNiQGpvc2VwaGJpbGxpbmdzbGV5LmNvLnVr';
-		var base_url = '//forms.brace.io/';
-		var action = base_url + atob(base64_email);
-
-		alert(action);
-		alert($('input[name=name]').val());
+function evolveTo(target, element) {
+	var len = element.innerHTML.length;
 		
-		$.ajax({
-			url: action,
-			method: "POST",
-			data: {
-				message: $('input[name=name]').val()
-			},
-			dataType: "json"
-		});
+	// Generate base with the same number of characters
+	var original = generateBase(len);
+	
+	element.innerHTML = original;
+	
+	var originalFitness = fitnessEvaluation(original, target);
+	
+	if (originalFitness > 0) {
+		window.setTimeout(evolveStep, initialWait, original, target, originalFitness, originalFitness, element);
+	}
+}
 
-		event.preventDefault();
-	});
-});
+function evolveStep(current, target, prevFitness, maxFitness, element) {
+	
+	// Pick a random position
+	var pos = randUnder(current.length);
+		
+	// Increment or decrement it
+	var change = randUnder(2) ? 1 : -1;
+	var newChar = nextChar(current.charAt(pos), change);
+		
+	// Replace it in the original string
+	current = setCharAt(pos, newChar, current);
+		
+	// Evaluate it for fitness and if it is better replace it
+	var newFitness = fitnessEvaluation(current, target);
+	
+	var currentWait = (1 - (newFitness / maxFitness)) * maxWait;
+	
+	if (newFitness < prevFitness) {
+		
+		element.innerHTML = current;
+		
+		if (newFitness > 0) {
+			prevFitness = newFitness;
+			window.setTimeout(evolveStep, currentWait, current, target, newFitness, maxFitness, element);
+		}
+	} else {
+		evolveStep(element.innerHTML, target, prevFitness, maxFitness, element)
+	}
+}
+
+function nextChar(c, i) {
+    return String.fromCharCode(c.charCodeAt(0) + i);
+}
+
+function setCharAt(index, character, candidate) {
+	return candidate.substr(0, index) + character + candidate.substr(index + character.length);
+}
+
+function generateBase(len) {
+	var allowed = "abcdefghijklmnopqrstuvwxyz";
+	var text = "";
+
+    for (var i = 0; i < len; i++) {
+        text += allowed.charAt(randUnder(allowed.length));
+	}
+
+    return text;
+}
+
+function fitnessEvaluation(current, target) {
+	var fitness = 0;
+	
+	// See how far each character is from the ideal
+	for (var i = 0; i < current.length; i++) {
+		fitness += Math.abs(target.charCodeAt(i) - current.charCodeAt(i));
+	}
+
+	return fitness;
+}
+
+function randUnder(len) {
+	return Math.floor(Math.random() * len)
+}
