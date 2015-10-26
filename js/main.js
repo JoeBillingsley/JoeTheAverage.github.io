83f13231bf15030748880e77d86a9d81181a8460
Joe Billingsley <jb@josephbillingsley.co.uk>
2015-10-26 13:03:49 +0000

    Added background image loading.

--- js/main.js
+++ js/main.js
@@ -1,87 +1,34 @@
-var evolvingElements = document.getElementsByClassName("evolve");
+var src 
+	= "http://blogs.birmingham.k12.mi.us/piercewearetheworld/files/2013/11/world-map-19mn2mn.gif";
 
-var initialWait = 5;
-var maxWait = 100;
-
-for(var i = 0; i < evolvingElements.length; i++) {	
-	evolveTo(evolvingElements[i].getAttribute("data-target"), evolvingElements[i]);
+function onLoad() {
+	var hero = document.getElementById("hero-background");
+	
+	hero.style.backgroundImage = 'url(' + src + ')';
+	hero.className += " fade-in";
+	
+	var role = document.getElementById("role-content");
+	
+	role.textContent = "";
+	
+	updateRole(role, "Hello world!", 0);
 }
 
-function evolveTo(target, element) {
-	var len = target.length;
+function updateRole(target, newText, i) {
 		
-	// Generate base with the same number of characters
-	var original = generateBase(len);
-	
-	element.innerHTML = original;
-	
-	var originalFitness = fitnessEvaluation(original, target);
-	
-	if (originalFitness > 0) {
-		window.setTimeout(evolveStep, initialWait, original, target, originalFitness, originalFitness, element);
+	if(i < newText.length) {
+		
+		setTimeout(function() {
+			
+			target.textContent += newText[i];
+			updateRole(target, newText, ++i);
+			
+		}, 100);
 	}
 }
 
-function evolveStep(current, target, prevFitness, maxFitness, element) {
-	
-	// Pick a random position
-	var pos = randUnder(current.length);
-		
-	// Increment or decrement it
-	var change = randUnder(2) ? 1 : -1;
-	var newChar = nextChar(current.charAt(pos), change);
-		
-	// Replace it in the original string
-	current = setCharAt(pos, newChar, current);
-		
-	// Evaluate it for fitness and if it is better replace it
-	var newFitness = fitnessEvaluation(current, target);
-	
-	var currentWait = (1 - (newFitness / maxFitness)) * maxWait;
-	
-	if (newFitness < prevFitness) {
-		
-		element.innerHTML = current;
-		
-		if (newFitness > 0) {
-			prevFitness = newFitness;
-			window.setTimeout(evolveStep, currentWait, current, target, newFitness, maxFitness, element);
-		}
-	} else {
-		evolveStep(element.innerHTML, target, prevFitness, maxFitness, element)
-	}
-}
-
-function nextChar(c, i) {
-    return String.fromCharCode(c.charCodeAt(0) + i);
-}
-
-function setCharAt(index, character, candidate) {
-	return candidate.substr(0, index) + character + candidate.substr(index + character.length);
-}
-
-function generateBase(len) {
-	var allowed = "abcdefghijklmnopqrstuvwxyz";
-	var text = "";
-
-    for (var i = 0; i < len; i++) {
-        text += allowed.charAt(randUnder(allowed.length));
-	}
-
-    return text;
-}
-
-function fitnessEvaluation(current, target) {
-	var fitness = 0;
-	
-	// See how far each character is from the ideal
-	for (var i = 0; i < current.length; i++) {
-		fitness += Math.abs(target.charCodeAt(i) - current.charCodeAt(i));
-	}
-
-	return fitness;
-}
-
-function randUnder(len) {
-	return Math.floor(Math.random() * len)
-}
+// Cache the image
+var image = new Image();
+image.onload = onLoad;
+image.onerror = onLoad;
+image.src = src;
